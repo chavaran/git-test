720e50f072c956ae88c071c12bd0447ab7294687
chavaran <56621754+chavaran@users.noreply.github.com>
2020-02-14 14:31:26 +0530

    Add files via upload

---
+++ TestAction.java
@@ -0,0 +1,33 @@
+package com.uob.fm.web.action.vault;
+
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+public class TestAction extends BaseDispatchAction {
+	private static final Logger logger = Logger.getLogger(VaultQueueAction.class);
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String accessCheckViolated = (String) request.getAttribute(AppConstants.ACCESS_VIOLATED);
+		PrintWriter out = response.getWriter();
+		if (accessCheckViolated != null && accessCheckViolated.equalsIgnoreCase(AppConstants.YES)) {
+			// For Ajax Calls
+			if (request.getHeader("x-requested-with") != null && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
+				StringBuffer strBuffer = new StringBuffer("{errorMsg: ");
+				strBuffer.append("'");
+				strBuffer.append("NO ACCESS TO THE OPERATION");
+				strBuffer.append("'");
+				strBuffer.append("}");
+				out.print(strBuffer.toString());
+				return null;
+			} else {
+				throw new Exception("static.userfunction.accessviolation");
+			}
+		}
+		return super.execute(mapping, form, request, response);
+	}
+
+}
